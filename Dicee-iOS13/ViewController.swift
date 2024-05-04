4a79ebcd73af5b063f80cc4b3ae3b5dca31f4d0a
lilith.avdalyan <lilith.avdalyan@altacode.com>
2024-05-05 01:39:34 +0400

    Initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // ctr + take coursor to controller code page and name the connection
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var dices = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix"),
+    ]
+    //then override the viewDidLoad method.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //make first dice half transparrent, and assign respective images to image views
+//        diceImageView1.alpha = 0.5
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton ) {
+        diceImageView1.image = dices.randomElement()
+        
+        // WHO.WHAT = VALUE
+        diceImageView2.image = dices.randomElement()
+    }
+    
+    
 }
 
